60ff113e1425f683e54f29e4fd18e36567f52f69
Jakub Krol <jkrol@sointeractive.pl>
2017-08-08 13:39:25 +0200

    add test for package install

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,7 +1,19 @@
 from testinfra.utils.ansible_runner import AnsibleRunner
+import yaml
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+defaults = yaml.load(open('defaults/main.yml', 'r'))
 
 
 def test_linux(SystemInfo):
     assert SystemInfo.type == 'linux'
+
+
+def test_packages(Package):
+    present = [
+        "oracle-java8-installer"
+    ]
+    if present and not defaults['java_user_install']:
+        for package in present:
+            p = Package(package)
+            assert p.is_installed
